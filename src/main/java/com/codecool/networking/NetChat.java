afba825e73c58b34f617864646eb9f722a1d3a3d
lpelczar <lukepelczar@gmail.com>
2018-06-20 14:44:21 +0200

    Add information about console arguments

--- src/main/java/com/codecool/networking/NetChat.java
+++ src/main/java/com/codecool/networking/NetChat.java
@@ -3,5 +3,12 @@ package com.codecool.networking;
 public class NetChat {
     public static void main(String[] args) {
 
+        if (args.length == 0) {
+            System.out.println("You need to provide options in following order 'java NetChat mode [address] port':" +
+                    "\nmode -> client or server" +
+                    "\naddress -> only if you are using client" +
+                    "\nport");
+            return;
+        }
     }
 }
