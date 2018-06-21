e38a70b2ad18db3e9e167ed85fbdc285e26d9450
lpelczar <lukepelczar@gmail.com>
2018-06-21 17:16:00 +0200

    Add thread pool

--- src/main/java/com/codecool/networking/modes/Server.java
+++ src/main/java/com/codecool/networking/modes/Server.java
@@ -3,6 +3,8 @@ package com.codecool.networking.modes;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server implements Runnable {
 
@@ -10,6 +12,7 @@ public class Server implements Runnable {
     private ServerSocket serverSocket;
     private boolean isStopped = false;
     private Thread runningThread;
+    private ExecutorService threadPool = Executors.newFixedThreadPool(10);
 
     Server(int port) {
         this.serverPort = port;
@@ -24,20 +27,20 @@ public class Server implements Runnable {
 
         openServerSocket();
 
-        while(!isStopped()){
+        while (!isStopped()) {
             Socket clientSocket = null;
             try {
                 clientSocket = this.serverSocket.accept();
             } catch (IOException e) {
-                if(isStopped()) {
+                if (isStopped()) {
                     System.out.println("Server Stopped.") ;
                     return;
                 }
-                throw new RuntimeException(
-                        "Error accepting client connection", e);
+                throw new RuntimeException("Error accepting client connection", e);
             }
-                // TODO 1: create new worker thread
+            this.threadPool.execute();
         }
+        this.threadPool.shutdown();
         System.out.println("Server Stopped.") ;
     }
 
