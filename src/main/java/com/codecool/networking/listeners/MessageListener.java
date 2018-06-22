690b1a18312b4f9c3ac54c7b4a3c00807d2d8771
lpelczar <lukepelczar@gmail.com>
2018-06-22 14:23:04 +0200

    Implement message listener runnable

---
+++ src/main/java/com/codecool/networking/listeners/MessageListener.java
@@ -0,0 +1,40 @@
+package com.codecool.networking.listeners;
+
+import com.codecool.networking.data.Message;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class MessageListener implements Runnable {
+
+    private ObjectInputStream is;
+    private boolean isStopped;
+
+    public MessageListener(ObjectInputStream is) {
+        this.is = is;
+    }
+
+    @Override
+    public void run() {
+
+        while (!isStopped()) {
+
+            try {
+                Message receivedMessage = (Message) is.readObject();
+                System.out.println(receivedMessage.getAuthor() + "> " + receivedMessage.getContent());
+            } catch (IOException | ClassNotFoundException e) {
+                // Ignore
+            }
+
+        }
+
+    }
+
+    private synchronized boolean isStopped() {
+        return this.isStopped;
+    }
+
+    public synchronized void stop(){
+        this.isStopped = true;
+    }
+}
