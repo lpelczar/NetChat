8ddb873dcf01338ead4cff843a86022f4eb42b13
lpelczar <lukepelczar@gmail.com>
2018-06-22 17:05:28 +0200

    Add message sender runnable class

---
+++ src/main/java/com/codecool/networking/runnables/MessageSender.java
@@ -0,0 +1,45 @@
+package com.codecool.networking.runnables;
+
+import com.codecool.networking.data.Message;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+
+public class MessageSender implements Runnable {
+
+    private String name;
+    private ObjectOutputStream os;
+    private boolean isStopped;
+
+    public MessageSender(ObjectOutputStream os, String name) {
+        this.os = os;
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println("* Type your message:");
+
+        while (!isStopped()) {
+
+            try {
+                String messageString = new Scanner(System.in).nextLine();
+                Message message = new Message(messageString, name);
+                System.out.println("you> " + messageString);
+                os.writeObject(message);
+            } catch (IOException e) {
+                stop();
+            }
+
+        }
+    }
+
+    public synchronized boolean isStopped() {
+        return this.isStopped;
+    }
+
+    public synchronized void stop(){
+        this.isStopped = true;
+    }
+}
