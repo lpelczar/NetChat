d2cade0e1c7fdc4e54d3315983004152bcfef85a
lpelczar <lukepelczar@gmail.com>
2018-06-22 19:01:45 +0200

    Add client message sender runnable

---
+++ src/main/java/com/codecool/networking/runnables/ClientMessageSender.java
@@ -0,0 +1,48 @@
+package com.codecool.networking.runnables;
+
+import com.codecool.networking.data.Message;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+
+public class ClientMessageSender implements Runnable {
+
+    private String name;
+    private ObjectOutputStream os;
+    private boolean isStopped;
+
+    public ClientMessageSender(ObjectOutputStream os, String name) {
+        this.name = name;
+        this.os = os;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("* Type your message:");
+
+        while (!isStopped()) {
+            String messageString = "";
+            while (!messageString.equals(".quit!")) {
+
+                messageString = new Scanner(System.in).nextLine();
+                if (!messageString.equals(".quit!")) {
+                    Message message = new Message(messageString, name);
+                    try {
+                        os.writeObject(message);
+                    } catch (IOException e) {
+                        stop();
+                    }
+                }
+            }
+        }
+    }
+
+    public synchronized boolean isStopped() {
+        return this.isStopped;
+    }
+
+    public synchronized void stop(){
+        this.isStopped = true;
+    }
+}
